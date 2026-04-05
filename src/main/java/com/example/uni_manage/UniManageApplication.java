3b6dd2e045491e9ac171b37b87be7fc6646abd65
Sethmindu Himsara <himsaragallewatta@gmail.com>
2026-04-05 22:09:53 +0530

    first commit

---
+++ src/main/java/com/example/uni_manage/UniManageApplication.java
@@ -0,0 +1,13 @@
+package com.example.uni_manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniManageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/uni_manage/UniManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uni_manage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniManageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
